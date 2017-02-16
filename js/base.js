8b92103a0a039ab7a2260f7d965820729f598fb0
Justin Zhao <252140484@qq.com>
2017-02-16 18:29:04 +0800

    finish detail task function

--- js/base.js
+++ js/base.js
@@ -7,7 +7,10 @@
       $task_detail=$('.task-detail'),
       $task_list_mask=$('.task-detail-mask'),
       $delete_task,
-      current_index;
+      current_index,
+      $update_form,
+      $task_detail_content,
+      $task_detail_content_input;
    init(); 
   // 点击添加事件    
   $form_add_task.on('submit',function(e){
@@ -113,16 +116,16 @@
 
   function update_task(index,data){
     if(!index||!task_list[index]) return;
-    task_list[index]=$.merge({},task_list[index],data);
+    task_list[index]=data;//$.merge({},task_list[index],data)
     refresh_task_list();
   }
-  console.log($('.task-detail'));
-  $('.task-list').on('click',function(){
-      console.log("click");
+  
+  // $('.task-list').on('click',function(){
+  //     console.log("click");
       
-      show_task_detail(current_index);
+  //     show_task_detail(current_index);
 
-  });
+  // });
   function hide_task_detail(){
     $task_detail.hide();
     $task_list_mask.hide();
@@ -132,19 +135,48 @@
   function render_task_detail(index){
     if(index===undefined||!task_list[index]) return;
     var item=task_list[index];
-  
-    var tpl='<form>'+'<div class="content">'+item.content+'</div>'+'<div>'+'<div class="desc">'+ 
-      '<textarea name="desc" id="" value="'+item.desc+'"></textarea>'+   
+    console.log(task_list);
+    console.log(item.content);
+    var tpl='<form>'+
+    '<div class="content">'+item.content+
+    '</div>'+
+    '<div>'+
+    '<input type="text" style="display:none;" name="content" value="'+
+     item.content+'">'+
+     '</div>'+
+    '<div>'+
+    '<div class="desc">'+ 
+      '<textarea name="desc">'+item.desc+'</textarea>'+   
     '</div>'+
     '</div>'+
     '<div class="remind">'+
-      '<input type="date" name="remind">'+
+      '<input type="date" name="remind" value="'+item.remind_date+'">'+
       '<button type="submit">Update</button>'+
     '</div>'+
     '</form>';
     $task_detail.html('');
     $task_detail.html(tpl);
+    $update_form=$task_detail.find('form');
+    $task_detail_content=$update_form.find('.content');
+    $task_detail_content_input=$update_form.find('[name=content]');
 
+    $task_detail_content.on('dblclick',function(){
+       $task_detail_content_input.show();
+       $task_detail_content.hide();
+    });
+
+    $update_form.on('submit',function(e){
+       e.preventDefault();
+       var data={};
+       data.content=$(this).find('[name=content]').val();
+       
+       data.desc=$(this).find('[name=desc]').val();
+       data.remind_date=$(this).find('[name=remind]').val();
+       console.log(data);
+       update_task(index,data);
+       hide_task_detail();
+    });
+    
  }
   // 渲染的語句
   function render_task_item(data,index){
